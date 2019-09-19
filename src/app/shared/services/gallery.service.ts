07f86606a9eb8dc6807692a4c4de8de2798da86a
Anoop <anoop@cedextech.com>
2019-09-19 10:17:43 +0530

    Added images set5

--- src/app/shared/services/gallery.service.ts
+++ src/app/shared/services/gallery.service.ts
@@ -43,131 +43,156 @@ export class GalleryService {
     },
     {
       id: 8,
+      name: 'Dates',
+      imageUrl: 'assets/images/fruits/dates.jpg'
+    },
+    {
+      id: 9,
       name: 'Dragon Fruit',
       imageUrl: 'assets/images/fruits/dragonfruit.jpg'
     },
     {
-      id: 9,
+      id: 10,
       name: 'Fig',
       imageUrl: 'assets/images/fruits/figs.jpg'
     },
     {
-      id: 10,
+      id: 11,
       name: 'Gooseberry',
       imageUrl: 'assets/images/fruits/gooseberry.jpg'
     },
     {
-      id: 11,
+      id: 12,
       name: 'Grape Fruit',
       imageUrl: 'assets/images/fruits/grapefruit.jpg'
     },
     {
-      id: 12,
+      id: 13,
       name: 'Black Grape',
       imageUrl: 'assets/images/fruits/grapes-black.jpg'
     },
     {
-      id: 13,
+      id: 14,
       name: 'Red Grape',
       imageUrl: 'assets/images/fruits/grapes-red.jpg'
     },
     {
-      id: 14,
+      id: 15,
       name: 'White Grape',
       imageUrl: 'assets/images/fruits/grapes-white.jpg'
     },
     {
-      id: 15,
+      id: 16,
       name: 'Jack Fruit',
       imageUrl: 'assets/images/fruits/jackfruit.png'
     },
     {
-      id: 16,
+      id: 17,
       name: 'Kiwi',
       imageUrl: 'assets/images/fruits/kiwi.jpg'
     },
     {
-      id: 17,
+      id: 18,
       name: 'Lemon',
       imageUrl: 'assets/images/fruits/lemons.jpg'
     },
     {
-      id: 18,
+      id: 19,
       name: 'Lychee',
       imageUrl: 'assets/images/fruits/lychee.jpg'
     },
     {
-      id: 19,
+      id: 20,
       name: 'Mango',
       imageUrl: 'assets/images/fruits/mangoes.jpg'
     },
     {
-      id: 20,
+      id: 21,
+      name: 'Mangosteen',
+      imageUrl: 'assets/images/fruits/mangosteen.jpg'
+    },
+    {
+      id: 22,
+      name: 'Mosambi',
+      imageUrl: 'assets/images/fruits/mosambi.jpg'
+    },
+    {
+      id: 23,
       name: 'Mulberry',
       imageUrl: 'assets/images/fruits/mulberries.jpg'
     },
     {
-      id: 21,
+      id: 24,
       name: 'Olive',
       imageUrl: 'assets/images/fruits/olives.jpg'
     },
     {
-      id: 22,
+      id: 25,
       name: 'Orange',
       imageUrl: 'assets/images/fruits/oranges.jpg'
     },
     {
-      id: 23,
+      id: 26,
       name: 'Papaya',
       imageUrl: 'assets/images/fruits/papaya.jpg'
     },
     {
-      id: 24,
+      id: 27,
       name: 'Passion Fruit',
       imageUrl: 'assets/images/fruits/passionfruit.jpg'
     },
     {
-      id: 25,
+      id: 28,
       name: 'Peach',
       imageUrl: 'assets/images/fruits/peach.jpg'
     },
     {
-      id: 26,
+      id: 29,
       name: 'Pear',
       imageUrl: 'assets/images/fruits/pears.jpg'
     },
     {
-      id: 27,
+      id: 30,
+      name: 'Pineapple',
+      imageUrl: 'assets/images/fruits/pineapples.jpg'
+    },
+    {
+      id: 31,
       name: 'Plums',
       imageUrl: 'assets/images/fruits/plums.jpg'
     },
     {
-      id: 28,
+      id: 32,
       name: 'Pomegranate',
       imageUrl: 'assets/images/fruits/pomegranates.jpg'
     },
     {
-      id: 29,
+      id: 33,
+      name: 'Rambutan',
+      imageUrl: 'assets/images/fruits/rambutan.jpg'
+    },
+    {
+      id: 34,
       name: 'Raspberry',
       imageUrl: 'assets/images/fruits/raspberries.jpg'
     },
     {
-      id: 30,
+      id: 35,
       name: 'Sapota',
       imageUrl: 'assets/images/fruits/sapota.jpg'
     },
     {
-      id: 31,
+      id: 36,
       name: 'shamam',
       imageUrl: 'assets/images/fruits/shamam.jpg'
     },
     {
-      id: 32,
+      id: 37,
       name: 'Strawberry',
       imageUrl: 'assets/images/fruits/strawberries.jpg'
     },
     {
-      id: 33,
+      id: 38,
       name: 'Watermelon',
       imageUrl: 'assets/images/fruits/watermelon.jpg'
     }
@@ -181,10 +206,3 @@ export class GalleryService {
 
 }
 
-/**
- * {
-      id: 2,
-      name: 'apples',
-      imageUrl: 'assets/images/fruits/apples.jpg'
-    }
- */
