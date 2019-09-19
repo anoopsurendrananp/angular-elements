caf4909b4dae6be272c9b040aaf1f53f2e672801
Anoop <anoop@cedextech.com>
2019-09-19 12:47:12 +0530

    Added images set6

--- src/app/shared/services/gallery.service.ts
+++ src/app/shared/services/gallery.service.ts
@@ -28,172 +28,195 @@ export class GalleryService {
     },
     {
       id: 5,
+      name: 'Bing Cherry',
+      imageUrl: 'assets/images/fruits/bing-cherries.jpg'
+    },
+    {
+      id: 6,
       name: 'Blackberry',
       imageUrl: 'assets/images/fruits/blackberries.jpg'
     },
     {
-      id: 6,
+      id: 7,
       name: 'Blueberry',
       imageUrl: 'assets/images/fruits/blueberries.jpg'
     },
     {
-      id: 7,
-      name: 'Cherries',
+      id: 8,
+      name: 'Cherry',
       imageUrl: 'assets/images/fruits/cherries.jpg'
     },
     {
-      id: 8,
+      id: 9,
+      name: 'Custard Apple',
+      imageUrl: 'assets/images/fruits/custard-apple.jpg'
+    },
+    {
+      id: 10,
       name: 'Dates',
       imageUrl: 'assets/images/fruits/dates.jpg'
     },
     {
-      id: 9,
+      id: 11,
       name: 'Dragon Fruit',
       imageUrl: 'assets/images/fruits/dragonfruit.jpg'
     },
     {
-      id: 10,
+      id: 12,
+      name: 'Egg Fruit',
+      imageUrl: 'assets/images/fruits/egg-fruits.jpg'
+    },
+    {
+      id: 13,
       name: 'Fig',
       imageUrl: 'assets/images/fruits/figs.jpg'
     },
     {
-      id: 11,
+      id: 14,
       name: 'Gooseberry',
       imageUrl: 'assets/images/fruits/gooseberry.jpg'
     },
     {
-      id: 12,
+      id: 15,
+      name: 'Grapes',
+      imageUrl: 'assets/images/fruits/grapes.jpg'
+    },
+    {
+      id: 16,
       name: 'Grape Fruit',
       imageUrl: 'assets/images/fruits/grapefruit.jpg'
     },
     {
-      id: 13,
-      name: 'Black Grape',
-      imageUrl: 'assets/images/fruits/grapes-black.jpg'
+      id: 17,
+      name: 'Guava',
+      imageUrl: 'assets/images/fruits/guavas.jpg'
     },
     {
-      id: 14,
-      name: 'Red Grape',
-      imageUrl: 'assets/images/fruits/grapes-red.jpg'
-    },
-    {
-      id: 15,
-      name: 'White Grape',
-      imageUrl: 'assets/images/fruits/grapes-white.jpg'
-    },
-    {
-      id: 16,
+      id: 18,
       name: 'Jack Fruit',
       imageUrl: 'assets/images/fruits/jackfruit.png'
     },
     {
-      id: 17,
+      id: 19,
+      name: 'Jamba',
+      imageUrl: 'assets/images/fruits/jamba.jpg'
+    },
+    {
+      id: 20,
       name: 'Kiwi',
       imageUrl: 'assets/images/fruits/kiwi.jpg'
     },
     {
-      id: 18,
+      id: 21,
       name: 'Lemon',
       imageUrl: 'assets/images/fruits/lemons.jpg'
     },
     {
-      id: 19,
+      id: 22,
       name: 'Lychee',
       imageUrl: 'assets/images/fruits/lychee.jpg'
     },
     {
-      id: 20,
+      id: 23,
       name: 'Mango',
+      scientificName: 'Mangifera indica',
       imageUrl: 'assets/images/fruits/mangoes.jpg'
     },
     {
-      id: 21,
+      id: 24,
       name: 'Mangosteen',
       imageUrl: 'assets/images/fruits/mangosteen.jpg'
     },
     {
-      id: 22,
+      id: 25,
       name: 'Mosambi',
       imageUrl: 'assets/images/fruits/mosambi.jpg'
     },
     {
-      id: 23,
+      id: 26,
       name: 'Mulberry',
       imageUrl: 'assets/images/fruits/mulberries.jpg'
     },
     {
-      id: 24,
+      id: 27,
       name: 'Olive',
       imageUrl: 'assets/images/fruits/olives.jpg'
     },
     {
-      id: 25,
+      id: 28,
       name: 'Orange',
       imageUrl: 'assets/images/fruits/oranges.jpg'
     },
     {
-      id: 26,
+      id: 29,
       name: 'Papaya',
       imageUrl: 'assets/images/fruits/papaya.jpg'
     },
     {
-      id: 27,
+      id: 30,
       name: 'Passion Fruit',
       imageUrl: 'assets/images/fruits/passionfruit.jpg'
     },
     {
-      id: 28,
+      id: 31,
       name: 'Peach',
       imageUrl: 'assets/images/fruits/peach.jpg'
     },
     {
-      id: 29,
+      id: 32,
       name: 'Pear',
       imageUrl: 'assets/images/fruits/pears.jpg'
     },
     {
-      id: 30,
+      id: 33,
       name: 'Pineapple',
       imageUrl: 'assets/images/fruits/pineapples.jpg'
     },
     {
-      id: 31,
+      id: 34,
       name: 'Plums',
       imageUrl: 'assets/images/fruits/plums.jpg'
     },
     {
-      id: 32,
+      id: 35,
       name: 'Pomegranate',
       imageUrl: 'assets/images/fruits/pomegranates.jpg'
     },
     {
-      id: 33,
+      id: 36,
       name: 'Rambutan',
       imageUrl: 'assets/images/fruits/rambutan.jpg'
     },
     {
-      id: 34,
+      id: 37,
       name: 'Raspberry',
       imageUrl: 'assets/images/fruits/raspberries.jpg'
     },
     {
-      id: 35,
+      id: 38,
       name: 'Sapota',
       imageUrl: 'assets/images/fruits/sapota.jpg'
     },
     {
-      id: 36,
+      id: 39,
       name: 'shamam',
       imageUrl: 'assets/images/fruits/shamam.jpg'
     },
     {
-      id: 37,
+      id: 40,
+      name: 'Star Fruit',
+      scientificName: 'Averrhoa carambola',
+      imageUrl: 'assets/images/fruits/starfruit.jpg'
+    },
+    {
+      id: 41,
       name: 'Strawberry',
       imageUrl: 'assets/images/fruits/strawberries.jpg'
     },
     {
-      id: 38,
+      id: 42,
       name: 'Watermelon',
+      scientificName: 'Citrullus lanatus',
       imageUrl: 'assets/images/fruits/watermelon.jpg'
     }
   ]
