42a3ccc3387a51b427464a1b2fbc73f5ebdabf7e
Anoop <anoop@cedextech.com>
2019-09-20 12:44:12 +0530

    Added font-awsome support

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   tooltipOptions = {
-    placement: 'bottom',
+    placement: 'left',
     theme: 'light'
   }
 
